5d259006d98a7a523997566e77a27f3d1e6659c0
satyendrasingh8 <satyendrasinghsss7398@gmail.com>
2021-01-04 00:05:19 +0530

    add dummy data and create model

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/helper/data.dart';
+import 'package:news_app/models/category_model.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,6 +8,13 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<CategoryModel> categories = new List<CategoryModel>();
+  @override
+  void initState() {
+    super.initState();
+    categories = getCategories();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +32,27 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         elevation: 0.0,
       ),
-      body: Container(),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              height: 70,
+              child: ListView.builder(
+                itemCount: categories.length,
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return CategoryTile(
+                    categoryName: categories[index].categoryName,
+                    imageUrl: categories[index].imageUrl,
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
@@ -35,11 +64,32 @@ class CategoryTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.only(right: 12),
       child: Stack(
         children: [
-          Image.network(imageUrl,width: 120,height: 70,),
-          
-          ],
+          ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Image.network(
+              imageUrl,
+              width: 120,
+              height: 70,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            width: 120,
+            height: 60,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(6),
+              color: Colors.black26 ),
+            alignment: Alignment.center,
+            child: Text(categoryName,style: TextStyle(color: Colors.white,
+            fontSize: 17
+            ),
+            textAlign: TextAlign.center,
+            ),
+          )
+        ],
       ),
     );
   }
